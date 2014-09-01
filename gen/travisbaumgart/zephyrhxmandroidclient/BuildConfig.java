2f8a996c8da56dd8e3d3366b641cee59341c288b
Travis Baumgart <baumgartt@bit-sys.com>
2014-08-31 22:45:48 -0600

    removing bin and gen dirs...

--- gen/travisbaumgart/zephyrhxmandroidclient/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package travisbaumgart.zephyrhxmandroidclient;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
