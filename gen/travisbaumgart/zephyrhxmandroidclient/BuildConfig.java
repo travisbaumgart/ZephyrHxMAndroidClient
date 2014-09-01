8ed6fe117915aa8898347cc9452a437c2132e38d
Travis Baumgart <baumgartt@bit-sys.com>
2014-08-31 22:43:53 -0600

    Initial checkin.  Project generated w/ Eclipse: Version: Kepler Service Release 2 Build id: 20140224-0627

---
+++ gen/travisbaumgart/zephyrhxmandroidclient/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package travisbaumgart.zephyrhxmandroidclient;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
