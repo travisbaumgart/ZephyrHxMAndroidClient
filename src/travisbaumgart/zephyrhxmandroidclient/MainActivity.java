971609c55f2c2b89d4b9df70b9c70d2cec1b5425
Travis Baumgart <baumgartt@bit-sys.com>
2014-09-12 15:52:12 -0600

    Adding basic UI elements to allow for an SVG based display.

--- src/travisbaumgart/zephyrhxmandroidclient/MainActivity.java
+++ src/travisbaumgart/zephyrhxmandroidclient/MainActivity.java
@@ -1,7 +1,6 @@
 package travisbaumgart.zephyrhxmandroidclient;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +8,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.caverock.androidsvg.SVGImageView;
 
 public class MainActivity extends Activity {
 
@@ -18,10 +21,15 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		View view = findViewById(R.id.center);
+
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+
+		super.onCreate(savedInstanceState);
+
 	}
 
 	@Override
@@ -30,6 +38,13 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onWindowFocusChanged(boolean focus) {
+		super.onWindowFocusChanged(focus);
+		// get the imageviews width and height here
+
 	}
 
 	@Override
@@ -57,6 +72,34 @@ public class MainActivity extends Activity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_main, container,
 					false);
+		
+			
+			
+			RelativeLayout leftLayout = (RelativeLayout) rootView
+					.findViewById(R.id.left);
+			
+	        SVGImageView leftSvgImageView = new SVGImageView(rootView.getContext());
+	        leftSvgImageView.setImageAsset("red1.svg");
+	        leftLayout.addView(leftSvgImageView,
+	                       new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+	        
+	        RelativeLayout centerLayout = (RelativeLayout) rootView
+					.findViewById(R.id.center);
+			
+	        SVGImageView centerSvgImageView = new SVGImageView(rootView.getContext());
+	        centerSvgImageView.setImageAsset("red4.svg");
+	        centerLayout.addView(centerSvgImageView,
+	                       new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+	        
+	        RelativeLayout rightLayout = (RelativeLayout) rootView
+					.findViewById(R.id.right);
+	        
+	        SVGImageView rightSvgImageView = new SVGImageView(rootView.getContext());
+	        rightSvgImageView.setImageAsset("red5.svg");
+	        rightLayout.addView(rightSvgImageView,
+	                       new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+	       
+
 			return rootView;
 		}
 	}
